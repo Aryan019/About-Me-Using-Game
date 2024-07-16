a23e7d70c0d4f8bdda91f03c263212e19f41a90b
Aryan019 <aryan19v@gmail.com>
2024-07-17 00:05:45 +0530

    Adding in the final touches

---
+++ src/constants.js
@@ -0,0 +1,20 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. I work mostly in JavaScript(Backend Dev) and also a Game Developer.
+    I've made a couple of games in Unity and Godot. I also like Machine Learning and Python. Anyway regardless of the language, I just like programming.
+    Here is my <a href="https://github.com/Aryan019" target="_blank">Github</a>!`,
+  "cs-degree": `This is my CS degree. I hung it on the wall and it reminds me of how my journey on the tech was started. It was a very theoretical degree but I think it gave me a good foundation.`,
+  "sofa-table": `That's my sofa. I like to relax here and watch YouTube or Football(btw who doesn't love this insane game). Watching TV from here keeps me entertain and helps me freshen my mood.`,
+  tv: `That's my TV. I've been watching tech youtubers a lot recently like :
+   <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">Theprimeagen</a>, <a href="https://www.youtube.com/@t3dotgg" target="_blank">Theo - t3.gg</a>,
+  <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a>`,
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+  resume: `This is my desk and on it is my resume. <a href="" target="_blank">Check it out?</a>
+  Contact me at aryan19v@gmail.com if you have any interesting job opportunities!`,
+  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas.`,
+  library: `There are a lot of programming books on my shelves.
+  I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
+  exit: `Close the tab to exit the game`,
+};
